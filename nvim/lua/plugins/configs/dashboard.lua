a0238e61ea8fda09bcc359a44efed38b3a0d7df2
Pranay Lad <142432489+Pranay-Lad@users.noreply.github.com>
2025-01-19 11:14:34 +0000

    Add files via upload

--- nvim/lua/plugins/configs/dashboard.lua
+++ nvim/lua/plugins/configs/dashboard.lua
@@ -4,7 +4,25 @@ return {
 	config = function()
 		require('dashboard').setup {
 			-- config
-			shortcut_type = 'number'
+			shortcut_type = 'number',
+			config = {
+				header = {""},
+				shortcut = {
+					{
+						icon = ' ',
+						icon_hl = '@variable',
+						desc = 'Files',
+						group = 'Label',
+						action = 'Telescope oldfiles',
+						key = 'f',
+					},
+				},
+				footer = {},
+				packages = { enable = false },
+				project = {enable = false},
+				mru = {enable = true, limit = 10, label=''},
+
+			}
 		}
 	end,
 	dependencies = { {'nvim-tree/nvim-web-devicons'}}
